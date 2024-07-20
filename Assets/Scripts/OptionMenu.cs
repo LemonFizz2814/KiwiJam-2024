8329a0783b804dd463962f339ab593242903fdb9
kesesek <126723293+kesesek@users.noreply.github.com>
2024-07-20 17:26:04 +1200

    add credits & options

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject OptionsPanel;
+    public Slider VolumeSlider;
+    public Dropdown WindowDropdown;
 
     public void OpenOptions()
     {
@@ -14,4 +16,41 @@ public class OptionsMenu : MonoBehaviour
     {
         OptionsPanel.SetActive(false);
     }
+
+    void Start()
+    {
+        VolumeSlider.value = PlayerPrefs.GetFloat("Volume", 50.0f);
+        WindowDropdown.value = PlayerPrefs.GetInt("Window", 1);
+
+        VolumeSlider.onValueChanged.AddListener(SetVolume);
+        WindowDropdown.onValueChanged.AddListener(SetWindow);
+    }
+
+    public void SetVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("Volume", volume);
+        AudioListener.volume = volume;
+        Debug.Log("Current Volume: " + volume);
+    }
+
+    public void SetWindow(int mode)
+    {
+        PlayerPrefs.SetInt("Window", mode);
+        switch (mode)
+        {
+            case 0:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 1:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+            case 2:
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+            default:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+        }
+    }
+
 }
